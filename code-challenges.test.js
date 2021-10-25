fe16d23509cfaedd73a7ff528b2f58577a556e15
Lilmcg <lilmcg@gmail.com>
2021-10-25 07:35:39 -0700

    completed week 6 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,11 +24,25 @@ var people = [
 ]
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe ("personCap", () => {
+  test("given an array of objects return an array with a sentence about each person with their name capitalized", () => {
+    expect(personCap(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
+// Ran test. Test failed. Error: personCap is not defined.
 
 // b) Create the function that makes the test pass.
 
+const personCap = (array) => {
+  return array.map(value => {
+    let newString = (value.name).toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
+    return newString + " " + `is ${value.occupation}.`
+  })
+}
+// console.log(personCap(people))
 
+// Ran test again. Test passed!
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -39,11 +53,30 @@ var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe ("onlyNumbers", () => {
+  test("given a mixed array return an array of only the remainders of the number when divided by 3", () => {
+    expect(onlyNumbers(hodgepodge1)).toEqual([2, 0, -1, 0])
+    expect(onlyNumbers(hodgepodge2)).toEqual([2, 1, -1])
+  })
+})
 
+// Ran test. Test failed. Error message: onlyNumbers is not defined.
 
 // b) Create the function that makes the test pass.
 
+const onlyNumbers = (array) => {
+  let divideThree=[]
+  let newArray= array.filter(value=>typeof(value)==="number")
+   for(let i=0; i<newArray.length; i++){
+    let num = newArray[i] % 3
+      divideThree.push(num)
+  } return divideThree
+}
 
+// console.log(onlyNumbers(hodgepodge1))
+// console.log(onlyNumbers(hodgepodge2))
+
+// Ran Test again. Test passed!
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -54,6 +87,27 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe ("sumOfCubes", () => {
+  test("given an array of numbers return the sum of the numbers cubed", () => {
+    expect(sumOfCubes(cubeAndSum1)).toEqual(99)
+    expect(sumOfCubes(cubeAndSum2)).toEqual(1125)
+  })
+})
 
+// Ran test. Test failed. Error: sumOfCubes is not defined.
 
 // b) Create the function that makes the test pass.
+
+const sumOfCubes = (array) => {
+  let sumNum = 0
+  for(let i = 0; i <array.length; i++) {
+    array[i]=array[i]**3
+    sumNum+=array[i]
+  }
+  return sumNum
+}
+
+// console.log(sumOfCubes(cubeAndSum1))
+// console.log(sumOfCubes(cubeAndSum2))
+
+// Ran test again. Test passed!
